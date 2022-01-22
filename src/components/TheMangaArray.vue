c7bc732f8aff85d44a348a296447482613b4c3ef
natthanguilhot <72070680+natthanguilhot@users.noreply.github.com>
2022-01-22 14:53:15 +0100

    upd

--- src/components/TheMangaArray.vue
+++ src/components/TheMangaArray.vue
@@ -18,19 +18,19 @@
                     <th class="w-36 border">{{ priceFormat(manga.price) }}</th>
                     <th class="w-36 border">{{ manga.parution }}</th>
                     <th class="w-36 border relative">
-                        <button type="button" @click="modifyTomeReleased(index, '+')" class="btnPlus hover:!text-white hover:!bg-black dark:hover:!bg-white dark:hover:!text-gray-900">+</button>
-                        {{ manga.lastReleasedVolume }}
                         <button type="button" @click="modifyTomeReleased(index, '-')" class="btnMinus hover:!text-white hover:!bg-black dark:hover:!bg-white dark:hover:!text-gray-900">-</button>
+                        {{ manga.lastReleasedVolume }}
+                        <button type="button" @click="modifyTomeReleased(index, '+')" class="btnPlus hover:!text-white hover:!bg-black dark:hover:!bg-white dark:hover:!text-gray-900">+</button>
                     </th>
                     <th class="w-36 border relative">
-                        <button type="button" @click="modifyTomeBought(index, '+')" class="btnPlus hover:!text-white hover:!bg-black dark:hover:!bg-white dark:hover:!text-gray-900">+</button>
-                        {{ manga.lastBoughtVolume }}
                         <button type="button" @click="modifyTomeBought(index, '-')" class="btnMinus hover:!text-white hover:!bg-black dark:hover:!bg-white dark:hover:!text-gray-900">-</button>
+                        {{ manga.lastBoughtVolume }}
+                        <button type="button" @click="modifyTomeBought(index, '+')" class="btnPlus hover:!text-white hover:!bg-black dark:hover:!bg-white dark:hover:!text-gray-900">+</button>
                     </th>
                     <th class="w-36 border relative">
-                        <button type="button" @click="modifyTomeRead(index, '+')" class="btnPlus hover:!text-white hover:!bg-black dark:hover:!bg-white dark:hover:!text-gray-900">+</button>
-                        {{ manga.lastRead }}
                         <button type="button" @click="modifyTomeRead(index, '-')" class="btnMinus hover:!text-white hover:!bg-black dark:hover:!bg-white dark:hover:!text-gray-900">-</button>
+                        {{ manga.lastRead }}
+                        <button type="button" @click="modifyTomeRead(index, '+')" class="btnPlus hover:!text-white hover:!bg-black dark:hover:!bg-white dark:hover:!text-gray-900">+</button>
                     </th>
                     <!-- <th>
                         <router-link :to="{name: 'MangaDetails', params:{id: manga.id, name: manga.name}}" aria-label="Consulter les tomes" class="w-8 h-8 flex justify-center items-center text-black bg-white p-1 m-1 border border-black rounded-full hover:text-white hover:bg-black dark:bg-white dark:hover:bg-black group">
@@ -56,7 +56,7 @@ export default {
     name:"TheMangaArray",
     data(){
         return {
-            sort:true,
+            sort:null,
         }
     },
     methods:{
@@ -95,6 +95,9 @@ export default {
                     this.sort=false;
                     return array.sort(this.sortPrice);
                 case false:
+                    this.sort=true;
+                    return array.sort(this.sortPriceDecrease);
+                case null :
                     this.sort=true;
                     return array.sort(this.sortPriceDecrease);
             }
