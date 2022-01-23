1f3137c55ec15b647bad795279feca28f118fb72
natthanguilhot <72070680+natthanguilhot@users.noreply.github.com>
2022-01-23 13:03:52 +0100

    responsive + fonction tri prix et tome acheté

--- src/components/TheMangaArray.vue
+++ src/components/TheMangaArray.vue
@@ -1,52 +1,51 @@
 <template>
-    <div class="flex justify-center items-center overflow-hidden">
-        <table class="border-separate overflow-scroll">
+    <div class="flex justify-start items-center lg:justify-center lg:items-center overflow-x-scroll">
+        <table class="border-separate">
             <thead class="">
                 <tr>
-                    <th class="w-36 border">Nom</th>
-                    <th @click="sortByPrice($store.state.listManga)" class="w-36 border cursor-pointer">Prix <span v-show="sort == true" class="inline-block transform -rotate-90">></span><span v-show="sort == false" class="inline-block transform rotate-90">></span></th>
-                    <th class="w-36 border">Parution</th>
-                    <th @click="sortByLastBoughtVolume($store.state.listManga)" class="w-36 border">Tomes Sortis</th>
-                    <th class="w-36 border">Tomes Achetés</th>
-                    <th class="w-36 border">Tomes Lus</th>
+                    <th class="w-36 min-w-[144px] border">Nom</th>
+                    <th @click="sortByPrice($store.state.listManga)" class="w-36 min-w-[144px] border cursor-pointer">Prix <span v-show="sort == true" class="inline-block transform -rotate-90">></span><span v-show="sort == false" class="inline-block transform rotate-90">></span></th>
+                    <th class="w-36 min-w-[144px] border">Parution</th>
+                    <th @click="sortByLastBoughtVolume($store.state.listManga)" class="w-36 min-w-[144px] border">Tomes Sortis</th>
+                    <th class="w-36 min-w-[144px] border">Tomes Achetés</th>
+                    <th class="w-36 min-w-[144px] border">Tomes Lus</th>
                     <th></th>
                 </tr>
             </thead>
             <tbody v-if="$store.state.listManga.length > 0">
                 <tr v-for="(manga, index) in $store.state.listManga" :key="manga.id" class="hover:bg-gray-400 h-12">
-                    <th class="w-36 border">{{ manga.name }}</th>
-                    <th class="w-36 border">{{ priceFormat(manga.price) }}</th>
-                    <th class="w-36 border">{{ manga.parution }}</th>
-                    <th class="w-36 border relative">
+                    <th class="w-36 min-w-[144px] border">{{ manga.name }}</th>
+                    <td class="w-36 min-w-[144px] border">{{ priceFormat(manga.price) }}</td>
+                    <td class="w-36 min-w-[144px] border">{{ manga.parution }}</td>
+                    <td class="w-36 min-w-[144px] border relative">
                         <button type="button" @click="modifyTomeReleased(index, '-')" class="btnMinus hover:!text-white hover:!bg-black dark:hover:!bg-white dark:hover:!text-gray-900">-</button>
                         {{ manga.lastReleasedVolume }}
                         <button type="button" @click="modifyTomeReleased(index, '+')" class="btnPlus hover:!text-white hover:!bg-black dark:hover:!bg-white dark:hover:!text-gray-900">+</button>
-                    </th>
-                    <th class="w-36 border relative">
+                    </td>
+                    <td class="w-36 min-w-[144px] border relative">
                         <button type="button" @click="modifyTomeBought(index, '-')" class="btnMinus hover:!text-white hover:!bg-black dark:hover:!bg-white dark:hover:!text-gray-900">-</button>
                         {{ manga.lastBoughtVolume }}
                         <button type="button" @click="modifyTomeBought(index, '+')" class="btnPlus hover:!text-white hover:!bg-black dark:hover:!bg-white dark:hover:!text-gray-900">+</button>
-                    </th>
-                    <th class="w-36 border relative">
+                    </td>
+                    <td class="w-36 min-w-[144px] border relative">
                         <button type="button" @click="modifyTomeRead(index, '-')" class="btnMinus hover:!text-white hover:!bg-black dark:hover:!bg-white dark:hover:!text-gray-900">-</button>
                         {{ manga.lastRead }}
                         <button type="button" @click="modifyTomeRead(index, '+')" class="btnPlus hover:!text-white hover:!bg-black dark:hover:!bg-white dark:hover:!text-gray-900">+</button>
-                    </th>
-                    <!-- <th>
+                    </td>
+                    <!-- <td>
                         <router-link :to="{name: 'MangaDetails', params:{id: manga.id, name: manga.name}}" aria-label="Consulter les tomes" class="w-8 h-8 flex justify-center items-center text-black bg-white p-1 m-1 border border-black rounded-full hover:text-white hover:bg-black dark:bg-white dark:hover:bg-black group">
                         <i class="fas fa-search-plus group-hover:text-white"></i>
                         </router-link>
-                    </th> -->
-                    <th>
+                    </td> -->
+                    <td>
                         <button type="button" @click="deleteManga(index)" aria-label="Supprimer un manga" class="w-8 h-8 flex justify-center items-center text-red-500 bg-white p-1 m-1 border border-black rounded-full hover:bg-black dark:bg-white dark:hover:bg-black">
                             <i class="fas fa-times"></i>
                         </button>
-                    </th>
+                    </td>
                 </tr>
             </tbody>
         </table>
     </div>
-
 </template>
 
 <script>
@@ -57,7 +56,7 @@ export default {
     data(){
         return {
             sort:true,
-            sortLastBoughtVolume:true,
+            sortLastBoughtVolumeBool:true,
         }
     },
     methods:{
@@ -90,6 +89,16 @@ export default {
                 return 1;
             return 0;
         },
+        sortByPrice(array){
+            switch (this.sort) {
+                case true:
+                    this.sort=false;
+                    return array.sort(this.sortPrice);
+                case false:
+                    this.sort = true;
+                    return array.sort(this.sortPriceDecrease);
+            }
+        },
         sortLastBoughtVolume(a, b) {
             if (a.lastBoughtVolume < b.lastBoughtVolume)
                 return -1;
@@ -104,23 +113,13 @@ export default {
                 return 1;
             return 0;
         },
-        sortByPrice(array){
-            switch (this.sort) {
-                case true:
-                    this.sort=false;
-                    return array.sort(this.sortPrice);
-                case false:
-                    this.sort = true;
-                    return array.sort(this.sortPriceDecrease);
-            }
-        },
         sortByLastBoughtVolume(array){
-            switch (this.sort) {
+            switch (this.sortLastBoughtVolumeBool) {
                 case true:
-                    this.sortLastBoughtVolume=false;
+                    this.sortLastBoughtVolumeBool=false;
                     return array.sort(this.sortLastBoughtVolume);
                 case false:
-                    this.sortLastBoughtVolume = true;
+                    this.sortLastBoughtVolumeBool = true;
                     return array.sort(this.sortLastBoughtVolumeDecrease);
             }
         },
@@ -144,4 +143,11 @@ export default {
     transform: scale(0.45);
   }
 }
+// th {
+//     position:absolute;
+//     width: 144px;
+// }
+// td {
+//     margin-left: 144px;
+// }
 </style>
